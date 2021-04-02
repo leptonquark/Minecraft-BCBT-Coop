4c80ae4a0db012b65fb2a58d3ba78f29f9348382
Justin Salér <justin.saler.r@gmail.com>
2021-04-02 11:11:57 +0200

    Refactor of Behaviour Tree Class
    Store agent as self.agent and move methods into class

--- tree.py
+++ tree.py
@@ -6,112 +6,106 @@ from gathering import get_gathering_tools
 from sequence import Sequence
 
 
-def get_goal_tree(agent, goal):
-    tree = Selector(
-        "Obtain " + goal,
-        children=[
-            Equip(agent, items.DIAMOND_PICKAXE),
-            Craft(agent, items.DIAMOND_PICKAXE),
-            get_gather_tree(agent, [items.DIAMOND_ORE]),
-            get_iron_craft_tree(agent),
-            get_gather_tree(agent, [items.IRON_ORE]),
-            get_stone_craft_tree(agent),
-            get_gather_tree(agent, [items.STONE]),
-            get_wooden_craft_tree(agent),
-            get_gather_tree(agent, [items.LOG, items.LOG_2])
-        ]
-    )
-    tree.setup_with_descendants()
-    return tree
-
-
-def get_base_goal_tree(agent):
-    goals = ["stone_pickaxe"]  # REMOVE
-    children = [get_goal_tree(agent, goal) for goal in goals]
-    tree = Sequence(
-        "BaseGoalTree",
-        children=children
-    )
-    tree.setup_with_descendants()
-    return tree
-
-
-def get_base_tree(agent):
-    baseGoalTree = get_base_goal_tree(agent)
-    tree = Sequence(
-        "BaseTree",
-        children=[JumpIfStuck(agent), baseGoalTree]
-    )
-    tree.setup_with_descendants()
-    return tree
-
-
-def get_wooden_craft_tree(agent):
-    sub_tree = Sequence(
-        "Craft wood",
-        children=[
-            Craft(agent, items.PLANKS, 10),
-            Craft(agent, items.STICKS, 8),
-            Craft(agent, items.CRAFTING_TABLE),
-        ],
-    )
-    sub_tree.setup_with_descendants()
-    tree = Sequence(
-        "Craft wooden pickaxe",
-        children=[
-            sub_tree,
-            Craft(agent, items.WOODEN_PICKAXE)
-        ]
-    )
-    tree.setup_with_descendants()
-    return tree
-
-
-def get_stone_craft_tree(agent):
-    tree = Sequence(
-        "Craft Stone",
-        children=[
-            Craft(agent, items.FURNACE),
-            Craft(agent, items.STONE_PICKAXE)
-        ]
-    )
-    tree.setup_with_descendants()
-    return tree
-
-
-def get_iron_craft_tree(agent):
-    tree = Selector(
-        "Craft Iron",
-        children=[
-            Craft(agent, items.IRON_PICKAXE),
-            Melt(agent, items.IRON_INGOT, 3)
-        ]
-    )
-    tree.setup_with_descendants()
-    return tree
-
-
-def get_gather_tree(agent, material):
-    tool = get_gathering_tools(material)
-
-    children = []
-    children += [
-        PickupItem(agent, material),
-        MineMaterial(agent, material),
-        GoToMaterial(agent, material),
-        DigDownwardsToMaterial(agent, material)
-    ]
-    if tool:
-        children.append(Equip(agent, tool))
-
-    tree = Selector(
-        "Gather " + str(material),
-        children=children
-    )
-    tree.setup_with_descendants()
-    return tree
-
-
 class BehaviourTree:
     def __init__(self, agent):
-        self.root = get_base_tree(agent)
+        self.agent = agent
+        self.root = self.get_base_tree()
+
+    def get_goal_tree(self, goal):
+        tree = Selector(
+            "Obtain " + goal,
+            children=[
+                Equip(self.agent, items.DIAMOND_PICKAXE),
+                Craft(self.agent, items.DIAMOND_PICKAXE),
+                self.get_gather_tree([items.DIAMOND_ORE]),
+                self.get_iron_craft_tree(),
+                self.get_gather_tree([items.IRON_ORE]),
+                self.get_stone_craft_tree(),
+                self.get_gather_tree([items.STONE]),
+                self.get_wooden_craft_tree(),
+                self.get_gather_tree([items.LOG, items.LOG_2])
+            ]
+        )
+        tree.setup_with_descendants()
+        return tree
+
+    def get_base_goal_tree(self):
+        goals = ["stone_pickaxe"]  # REMOVE
+        children = [self.get_goal_tree(goal) for goal in goals]
+        tree = Sequence(
+            "BaseGoalTree",
+            children=children
+        )
+        tree.setup_with_descendants()
+        return tree
+
+    def get_base_tree(self):
+        baseGoalTree = self.get_base_goal_tree()
+        tree = Sequence(
+            "BaseTree",
+            children=[JumpIfStuck(self.agent), baseGoalTree]
+        )
+        tree.setup_with_descendants()
+        return tree
+
+    def get_wooden_craft_tree(self):
+        sub_tree = Sequence(
+            "Craft wood",
+            children=[
+                Craft(self.agent, items.PLANKS, 10),
+                Craft(self.agent, items.STICKS, 8),
+                Craft(self.agent, items.CRAFTING_TABLE),
+            ],
+        )
+        sub_tree.setup_with_descendants()
+        tree = Sequence(
+            "Craft wooden pickaxe",
+            children=[
+                sub_tree,
+                Craft(self.agent, items.WOODEN_PICKAXE)
+            ]
+        )
+        tree.setup_with_descendants()
+        return tree
+
+    def get_stone_craft_tree(self):
+        tree = Sequence(
+            "Craft Stone",
+            children=[
+                Craft(self.agent, items.FURNACE),
+                Craft(self.agent, items.STONE_PICKAXE)
+            ]
+        )
+        tree.setup_with_descendants()
+        return tree
+
+    def get_iron_craft_tree(self):
+        tree = Selector(
+            "Craft Iron",
+            children=[
+                Craft(self.agent, items.IRON_PICKAXE),
+                Melt(self.agent, items.IRON_INGOT, 3)
+            ]
+        )
+        tree.setup_with_descendants()
+        return tree
+
+    def get_gather_tree(self, material):
+        tool = get_gathering_tools(material)
+
+        children = []
+        children += [
+            PickupItem(self.agent, material),
+            MineMaterial(self.agent, material),
+            GoToMaterial(self.agent, material),
+            DigDownwardsToMaterial(self.agent, material)
+        ]
+        if tool:
+            children.append(Equip(self.agent, tool))
+
+        tree = Selector(
+            "Gather " + str(material),
+            children=children
+        )
+        tree.setup_with_descendants()
+        return tree
