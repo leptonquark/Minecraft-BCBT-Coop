c47c646e06e427787ae9b155708b910c558b0f0e
Justin Salér <justin.saler.r@gmail.com>
2022-12-04 18:16:13 +0100

    Move Agent property to Condition

--- bt/receiver.py
+++ bt/receiver.py
@@ -1,18 +1,22 @@
-from bt.conditions import Condition
+from py_trees.behaviour import Behaviour
+from py_trees.common import Status
 
 
-class Receiver(Condition):
+class Receiver(Behaviour):
     def __init__(self, blackboard, channel, value=True):
         super().__init__(f"Is {channel} fulfilled")
         self.blackboard = blackboard
         self.channel = channel
         self.value = value
 
+    def update(self):
+        return Status.SUCCESS if self.verify() else Status.FAILURE
+
     def verify(self):
         return self.blackboard.get(self.channel) == self.value
 
 
-class InverseReceiver(Condition):
+class InverseReceiver(Behaviour):
     def __init__(self, blackboard, channel, values=None):
         if values is None:
             values = [False]
@@ -21,6 +25,8 @@ class InverseReceiver(Condition):
         self.channel = channel
         self.values = values
 
+    def update(self):
+        return Status.SUCCESS if self.verify() else Status.FAILURE
+
     def verify(self):
-        # print(f"Receiver {self.channel} values:  {self.blackboard.get(self.channel)}")
         return self.blackboard.get(self.channel, False) not in self.values
