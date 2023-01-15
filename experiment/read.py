474d3dc3555ddd3a4bc444515688d2b490c489ee
Justin Salér <justin.saler.r@gmail.com>
2023-01-15 08:55:49 +0100

    Clean up read

--- experiment/read.py
+++ experiment/read.py
@@ -14,12 +14,7 @@ FULL_WIDTH = 0.8
 
 
 def plot_completion_times(fpp, flat_world):
-    if fpp and flat_world:
-        files = ["output_fwg_2.csv", "output_fwg_3.csv"]
-    elif fpp and not flat_world:
-        files = ["output_dwg_7.csv", "output_dwg_8.csv"]
-    else:
-        files = ["output_g10spmdw_5.csv"]
+    files = get_files(flat_world, fpp)
     dfs = [pd.read_csv(get_project_root() / EXPERIMENT_PATH / file).astype({"collaborative": str}) for file in files]
     df = pd.concat(dfs)
 
@@ -39,22 +34,7 @@ def plot_completion_times(fpp, flat_world):
     cooperativities = len(stats_full.collaborative.unique())
     width = FULL_WIDTH / cooperativities
     for without_edges in [False, True]:
-
-        def get_delta_x(collaborative):
-            if cooperativities == 3:
-                if collaborative == "Both":
-                    return width
-                elif collaborative == "True":
-                    return 0
-                else:
-                    return -width
-            else:
-                if collaborative == "True" or collaborative:
-                    return width / 2
-                else:
-                    return -width / 2
-
-        x = [row.agents + get_delta_x(row.collaborative) for _, row in stats_full.iterrows()]
+        x = [row.agents + get_delta_x(row.collaborative, cooperativities, width) for _, row in stats_full.iterrows()]
         palette = ['r', 'g', 'b']
         colors = [palette[row.agents - 1] for _, row in stats_full.iterrows()]
         fig, ax = plt.subplots()
@@ -75,24 +55,8 @@ def plot_completion_times(fpp, flat_world):
             capsize=CAPSIZE,
             width=width
         )
-
-        ticks = []
-
-        for t in x[::cooperativities]:
-            if cooperativities == 3:
-                ticks += [t - 2 * width, t - width + 0.0001, t - width, t]
-            elif cooperativities == 2:
-                ticks += [t, t + width / 2, t + width]
-
-        labels = []
-        if cooperativities == 2:
-            for agent in stats_full.agents.unique():
-                labels += ['Indep', f"\n{agent} {'agents' if agent > 1 else 'agent'}", 'Collab']
-        elif cooperativities == 3:
-            for agent in stats_full.agents.unique():
-                labels += ['Indep', f"\n{agent} {'agents' if agent > 1 else 'agent'}", 'Collab', "Both"]
-        plt.xticks(ticks)
-        ax.set_xticklabels(labels)
+        plt.xticks(get_ticks(cooperativities, width, x))
+        ax.set_xticklabels(get_labels(cooperativities, stats_full))
         ax.tick_params(axis='x', which='both', length=0)
 
         plt.ylabel('Average completion time (s)')
@@ -101,20 +65,76 @@ def plot_completion_times(fpp, flat_world):
         plt.ylim([0, ymax])
 
         runs = int(len(df) / (cooperativities * len(stats_full.agents.unique())))
-        if fpp:
-            generator = "flat world generator" if flat_world else "default world generator"
-            plt.title(f'{runs} runs each using {generator}')
-        else:
-            plt.title(f'{runs} runs each in the stone pickaxe scenario')
-
-        we = "we_" if without_edges else ""
-        if fpp:
-            gen = "fwg" if flat_world else "dwg"
-            filename = f"{gen}_{we}{runs}"
-        else:
-            filename = f"sp_{we}{runs}"
+        title = get_title(flat_world, fpp, runs)
+        plt.title(title)
+        filename = get_file_name(flat_world, fpp, runs, without_edges)
         plt.savefig(filename)
         plt.show()
+
+
+def get_title(flat_world, fpp, runs):
+    if fpp:
+        generator = "flat world generator" if flat_world else "default world generator"
+        title = f"{runs} runs each using {generator}"
+    else:
+        title = f"{runs} runs each in the stone pickaxe scenario"
+    return title
+
+
+def get_file_name(flat_world, fpp, runs, without_edges):
+    we = "we_" if without_edges else ""
+    if fpp:
+        gen = "fwg" if flat_world else "dwg"
+        filename = f"{gen}_{we}{runs}"
+    else:
+        filename = f"sp_{we}{runs}"
+    return filename
+
+
+def get_files(flat_world, fpp):
+    if fpp and flat_world:
+        files = ["output_fwg_2.csv", "output_fwg_3.csv"]
+    elif fpp and not flat_world:
+        files = ["output_dwg_7.csv", "output_dwg_8.csv"]
+    else:
+        files = ["output_g10spmdw_5.csv"]
+    return files
+
+
+def get_delta_x(collaborative, cooperativities, width):
+    if cooperativities == 3:
+        if collaborative == "Both":
+            return width
+        elif collaborative == "True":
+            return 0
+        else:
+            return -width
+    else:
+        if collaborative == "True" or collaborative:
+            return width / 2
+        else:
+            return -width / 2
+
+
+def get_labels(cooperativities, stats_full):
+    labels = []
+    if cooperativities == 2:
+        for agent in stats_full.agents.unique():
+            labels += ['Indep', f"\n{agent} {'agents' if agent > 1 else 'agent'}", 'Collab']
+    elif cooperativities == 3:
+        for agent in stats_full.agents.unique():
+            labels += ['Indep', f"\n{agent} {'agents' if agent > 1 else 'agent'}", 'Collab', "Both"]
+    return labels
+
+
+def get_ticks(cooperativities, width, x):
+    ticks = []
+    for t in x[::cooperativities]:
+        if cooperativities == 3:
+            ticks += [t - 2 * width, t - width + 0.0001, t - width, t]
+        elif cooperativities == 2:
+            ticks += [t, t + width / 2, t + width]
+    return ticks
 
 
 if __name__ == '__main__':
